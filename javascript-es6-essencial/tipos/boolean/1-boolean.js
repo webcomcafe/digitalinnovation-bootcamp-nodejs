3a1656a763252bae816368fe7ff36bdb82d1c68b
Airton Lopes <webcomcafe@outlook.com>
2020-08-06 22:56:40 -0300

    boolean, null, undefined

---
+++ javascript-es6-essencial/tipos/boolean/1-boolean.js
@@ -0,0 +1,6 @@
+const isActive = true;
+
+const isAuthenticated = false;
+
+console.log('Tipo da variável:', typeof isActive);
+console.log('Tipo da variável:', typeof isAuthenticated);
---
+++ javascript-es6-essencial/tipos/undefined/1-undefined.js
@@ -0,0 +1,3 @@
+let undefinedVariable;
+
+console.log('Tipo da variável é:', typeof undefinedVariable);
